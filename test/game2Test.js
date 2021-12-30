f6b3b67e7423614348bca2c73bcdd149aaf1ba56
A-F-V <af695@cam.ac.uk>
2021-12-30 19:44:32 +0000

    DONE

--- test/game2Test.js
+++ test/game2Test.js
@@ -1,11 +1,14 @@
 const { assert } = require("chai");
 
-describe("Game2", function() {
-  it("should be a winner", async function() {
+describe("Game2", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game2");
     const game = await Game.deploy();
     await game.deployed();
 
+    await game.switchOn(20);
+    await game.switchOn(47);
+    await game.switchOn(212);
     // press all the right switches to win this stage
 
     await game.win();
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,14 +1,39 @@
+const { decryptJsonWalletSync } = require("@ethersproject/json-wallets");
 const { assert } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
 
-describe("Game5", function() {
-  it("should be a winner", async function() {
+describe("Game5", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
     await game.deployed();
 
-    // good luck
+    const threshold = BigNumber.from(
+      "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf"
+    );
 
-    await game.win();
+    // MINING FOR SOME ACCOUNTS
+    let nonce = 1;
+    while (true) {
+      const pkey = new ethers.utils.SigningKey(nonce).publicKey;
+      const address = ethers.utils.computeAddress(pkey);
+      const number = BigNumber.from(address);
+      if (number.lt(threshold)) {
+        break;
+      }
+      nonce++;
+    }
+    const wallet = new ethers.Wallet(nonce, ethers.provider);
+
+    // good luck
+    const tx = await ethers.provider.getSigner(0).sendTransaction({
+      value: ethers.utils.parseEther("1"),
+      to: wallet.address,
+    });
+    await tx.wait();
+
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
